e09491cf4a5bd38bd31d733bdaced1ef91493a97
Jauerbach123 <224jauerbach@frhsd.com>
2023-03-29 09:33:23 -0400

    new

--- replit.nix
+++ replit.nix
@@ -1,6 +1,93 @@
-{ pkgs }: {
-  deps = [
-    pkgs.nodePackages.vscode-langservers-extracted
-    pkgs.nodePackages.typescript-language-server
-  ];
-}
+ <div id="section1" class=" text-white" style="padding:100px 20px;">
+    <h1>The Life of the Designer
+    </h1>
+    <br>
+    <br>
+    <br>
+    <div class="row">
+      <div class="col-sm-2"></div>
+      <div class="col-sm-5">
+        <div id="accordionNav">
+          <div class="accordionPanel">
+            <div class="accordionPanelTab" id="eventsWeekTab" onclick="showEventsWeek();">The Man Himself</div>
+            <div class="accordionPanelContent" id="eventsWeekContent">
+              <a href="#">
+                <img src="ferruccio3.jpg" width="400" height="280">
+                <h3>Modal 1: 350 GT</h3>
+              </a>
+              <a href="#">
+                <h3>Date: </h3>
+              </a>
+              <a href="#">
+                <h3>Wed: New York On Bikes</h3>
+              </a>
+              <a href="#">
+                <h3>Thu: Gerry and the Atrics</h3>
+              </a>
+              <a href="#">
+                <h3>Fri: Low Lustre</h3>
+              </a>
+            </div>
+          </div>
+          <div class="accordionPanel">
+            <div class="accordionPanelTab" id="comingUpTab" onclick="showComingup();">His First Child</div>
+            <div class="accordionPanelContent" id="comingUpContent">
+              <a class="eventContent">
+                <img src="1965-lamborghini-350gt-coupe-front.jpg" width="350" height="230"></h3>
+              </a>
+              <a>
+                <h3>Modal 1: 350 GT</h3>
+              </a>
+              <a href="#">
+                <h3>Date: May,1963</h3>
+              </a>
+              <a>
+                <h3>280 horsepower</h3>
+              </a>
+
+            </div>
+          </div>
+          <div class="accordionPanel">
+            <div class="accordionPanelTab" id="pastEventsTab" onclick="showPastEvents();">Where It All Began</div>
+            <div class="accordionPanelContent" id="pastEventsContent">
+              <a href="#">
+                <img src="Image920x600.jpg" width="400" height="240">
+
+              </a>
+
+              <a>
+                <h4>Headquartered: Sant'Agata Bolognese, Northern Italy</h4>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+    </div>
+      </div>
+
+      <div class="col-sm-5">
+
+        <div class="box">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <div class="center">
+            <h1> Bio</h1>
+            <p>
+              Ferruccio Lamborghini was an Italian entrepreneur who founded Lamborghini, a luxury sports car brand, in
+              1963. Prior to entering the automotive industry, he made his fortune as a manufacturer of tractors and air
+              conditioning units. Lamborghini's passion for cars led him to create his own line of high-performance
+              vehicles </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+
+
+
+  
